2d4c8ac66b25b368b2f8c0acb12a3afee72bea6c
cwangg897 <dhkdrb897@gmail.com>
2024-10-13 11:00:01 +0900

    Feat: common모듈 : 페이지네이션 응답포맷

---
+++ common/src/main/java/com/task/PageResult.java
@@ -0,0 +1,10 @@
+package com.task;
+
+import java.util.List;
+import lombok.Builder;
+
+// Spring Data Jpa에서는 응답기본객체를 지원해주지만 Mybatis나 다른db나 mapper를
+// 사용할 때는 이용하지못하기때문에 공통 포맷을 만들었습니다.
+@Builder
+public record PageResult<T>(int page, int size, int totalElements, List<T> contents) {
+}
