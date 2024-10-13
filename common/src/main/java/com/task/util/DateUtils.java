601aec77ba86d985ad775aae8f9885e0864b7ae0
cwangg897 <dhkdrb897@gmail.com>
2024-10-13 21:52:31 +0900

    Feat: 시간응답포맷

---
+++ common/src/main/java/com/task/util/DateUtils.java
@@ -0,0 +1,20 @@
+package com.task.util;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+    private static final DateTimeFormatter YYYY_MM_DD_HH_MM_SS_FORMATTER =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    // LocalDateTime을 한국 시간으로 변환하고 지정된 형식으로 반환하는 메서드
+    public static String formatToKoreaTime(LocalDateTime dateTime) {
+        // LocalDateTime을 한국 시간으로 변환
+        ZonedDateTime koreaTime = dateTime.atZone(ZoneId.of("Asia/Seoul"));
+
+        // 원하는 형식으로 포맷팅
+        return koreaTime.format(YYYY_MM_DD_HH_MM_SS_FORMATTER);
+    }
+}
