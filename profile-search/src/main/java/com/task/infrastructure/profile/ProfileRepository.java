cefcb14464cd48818d151560559e6ac0571b0451
Choi Wang Gyu <79621675+cwangg897@users.noreply.github.com>
2024-10-14 23:37:52 +0900

    Feat: CircuitBreaker패턴 resilience4j 적용 (#14)

--- profile-search/src/main/java/com/task/infrastructure/profile/ProfileRepository.java
+++ profile-search/src/main/java/com/task/infrastructure/profile/ProfileRepository.java
@@ -1,7 +1,8 @@
 package com.task.infrastructure.profile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ProfileRepository extends JpaRepository<ProfileEntity, Long>, ProfileRepositoryCustom {
 }
--- profile-search/src/main/java/com/task/service/profile/ProfileQueryServiceImpl.java
+++ profile-search/src/main/java/com/task/service/profile/ProfileQueryServiceImpl.java
@@ -6,7 +6,11 @@ import com.task.PageResult;
 import com.task.controller.response.ProfileResponse;
 import com.task.infrastructure.profile.ProfileEntity;
 import com.task.infrastructure.profile.ProfileRepository;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -14,11 +18,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ProfileQueryServiceImpl implements ProfileQueryService {
 
     private final ProfileRepository profileRepository;
 
+
+    // 서킷 적용하기
+    @CircuitBreaker(name = "ProfileGetById", fallbackMethod = "getByIdFallBack")
     @Cacheable(value = "profile", key = "#id")
     @Override
     public ProfileResponse getById(Long id) {
@@ -27,6 +35,17 @@ public class ProfileQueryServiceImpl implements ProfileQueryService {
                 new ApiException("profiles-search에서 ID "+ id + " 찾을 수 없습니다", ErrorType.NO_RESOURCE
             , HttpStatus.NOT_FOUND));
     }
+
+    // 로컬 캐시에 담기 - 서비스마다 로컬 캐시에 담긴 데이터가 다를 수 있다는 점을 주의해야 한다.
+    // 그런데 이는 db에 영향을 주어 전체적인 서비스에 영향을 줄 수 있기때문에 redis cluster나 sentinal로 고가용성을 해두기 때문에 이 시간동안 버티는 용도
+    public ProfileResponse getByIdFallBack(Long id, Throwable throwable){
+        log.warn("[ProfileQueryServiceImpl] Circuit Breaker is open! Fallback to DB search.");
+        return profileRepository.searchById(id)
+            .orElseThrow(()->
+                new ApiException("profiles-search에서 ID "+ id + " 찾을 수 없습니다", ErrorType.NO_RESOURCE
+                    , HttpStatus.NOT_FOUND));
+    }
+
 
     @Cacheable(cacheNames = "profiles", value = "profiles", key =
         "'page_' + #pageable.getOffset() + "
