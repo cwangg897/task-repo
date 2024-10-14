3aff9c5282c9f73be4909affdc130e11a065f03c
cwangg897 <dhkdrb897@gmail.com>
2024-10-14 15:08:57 +0900

    Feat: Redis Cache

--- profile-search/src/main/java/com/task/service/ProfileApplicationService.java
+++ profile-search/src/main/java/com/task/service/ProfileApplicationService.java
@@ -5,14 +5,13 @@ import com.task.ErrorType;
 import com.task.PageResult;
 import com.task.controller.response.ProfileResponse;
 import com.task.infrastructure.profile.ProfileEntity;
-import com.task.infrastructure.profile.QProfileEntity;
 import com.task.infrastructure.profile_stat.ProfileViewStatEntity;
 import com.task.service.profile.ProfileQueryService;
 import com.task.service.profile_stat.ProfileViewStatCommandService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 // 퍼사드 패턴
 @Service
@@ -22,6 +21,7 @@ public class ProfileApplicationService {
     private final ProfileQueryService profileQueryService;
     private final ProfileViewStatCommandService profileViewStatCommandService;
 
+    @Cacheable(value = "profile", key = "#id")
     public ProfileResponse getById(Long id) {
         ProfileResponse response = profileQueryService.getById(id);
         ProfileViewStatEntity viewStatEntity = new ProfileViewStatEntity(new ProfileEntity(
@@ -31,7 +31,14 @@ public class ProfileApplicationService {
         return response;
     }
 
+    @Cacheable(cacheNames = "profiles", value = "profiles", key = "'page_' + #pageable.getOffset() + "
+        + "'_size_' + #pageable.getPageSize() + '_sort_' + #pageable.sort.toString()")
     public PageResult<ProfileResponse> getAllByCondition(Pageable pageable) {
         return profileQueryService.getAllByCondition(pageable);
+    }
+
+    @Cacheable(key = "hello")
+    public String test() {
+        return "hello";
     }
 }
